71f810c673ce3b3641770574e22caf2b532e86c8
Алексей <kalininalexey2017@gmail.com>
2026-01-17 06:16:52 +0300

    Добавьте файлы проекта.

---
+++ Project PE/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Auth()
+{
+
+    public bool SignIn(string login, string password)
+    {
+        if (password == null || password.Length == 0)
+        {
+            throw new Exception();
+        }
+        return true;
+    }
+}
